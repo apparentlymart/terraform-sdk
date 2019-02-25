1e5124a77713b8816a3e1ba5e3bf9907cc6d07c3
Martin Atkins <mart@degeneration.co.uk>
2019-02-24 16:53:19 -0800

    Flesh out the Provider and resource type APIs a little more

--- provider.go
+++ provider.go
@@ -1,8 +1,47 @@
 package tfsdk
 
+import (
+	"context"
+
+	"github.com/zclconf/go-cty/cty"
+)
+
 // Provider is the main type for describing a Terraform provider
 // implementation. The primary Go package for a provider should include
 // a function that returns a pointer to a Provider object describing the
 // resource types and other objects exposed by the provider.
 type Provider struct {
+	ConfigSchema         *SchemaBlockType
+	ManagedResourceTypes map[string]ManagedResourceType
+	DataResourceType     map[string]DataResourceType
+
+	ConfigureFn interface{}
+}
+
+// ManagedResourceType is the interface implemented by managed resource type
+// implementations.
+//
+// This is a closed interface, meaning that all of its implementations are
+// inside this package. To implement a managed resource type, create a
+// *ResourceType value and pass it to NewManagedResourceType.
+type ManagedResourceType interface {
+	getSchema() *SchemaBlockType
+	validate(obj cty.Value) Diagnostics
+	upgradeState(oldJSON []byte, oldVersion int) (cty.Value, Diagnostics)
+	refresh(ctx context.Context, client interface{}, old cty.Value) (cty.Value, Diagnostics)
+	planChange(ctx context.Context, client interface{}, prior, config, proposed cty.Value) (cty.Value, Diagnostics)
+	applyChange(ctx context.Context, client interface{}, prior, config, planned cty.Value) (cty.Value, Diagnostics)
+	importState(ctx context.Context, client interface{}, id string) (cty.Value, Diagnostics)
+}
+
+// DataResourceType is an interface implemented by data resource type
+// implementations.
+//
+// This is a closed interface, meaning that all of its implementations are
+// inside this package. To implement a managed resource type, create a
+// *ResourceType value and pass it to NewDataResourceType.
+type DataResourceType interface {
+	getSchema() *SchemaBlockType
+	validate(obj cty.Value) Diagnostics
+	read(ctx context.Context, client interface{}, config cty.Value) (cty.Value, Diagnostics)
 }
