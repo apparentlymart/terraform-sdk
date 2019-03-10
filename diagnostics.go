598dc262459fd77c57d50f96a58492957cca895f
Martin Atkins <mart@degeneration.co.uk>
2019-03-10 09:35:48 -0700

    Move diagnostics types into a separate helper package
    We'll tend to need the diagnostics types and functions in other packages
    in the SDK that might be imported by the main tfsdk package, so having
    these separated out can avoid import cycles in that case.
    
    The public interface to these is still in the main package. The internal
    package the interface only for other packages within this module that need
    to return or otherwise work with diagnostics without importing the main
    tfsdk package.

--- diagnostics.go
+++ diagnostics.go
@@ -1,9 +1,7 @@
 package tfsdk
 
 import (
-	"fmt"
-	"strings"
-
+	"github.com/apparentlymart/terraform-sdk/internal/sdkdiags"
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -13,7 +11,7 @@ import (
 // A nil Diagnostics indicates no problems. However, a non-nil Diagnostics
 // may contain only warnings, so use method HasErrors to recognize when an
 // error has occurred.
-type Diagnostics []Diagnostic
+type Diagnostics = sdkdiags.Diagnostics
 
 // Diagnostic represents a single problem that occurred during an operation.
 //
@@ -26,122 +24,32 @@ type Diagnostics []Diagnostic
 // to produce more specific problem reports, possibly containing direct
 // references to the problematic value. General problems, such as total
 // inability to reach a remote API, should be reported with a nil Path.
-type Diagnostic struct {
-	Severity DiagSeverity
-	Summary  string
-	Detail   string
-	Path     cty.Path
-}
+type Diagnostic = sdkdiags.Diagnostic
 
-func (diags Diagnostics) Append(vals ...interface{}) Diagnostics {
-	for _, rawVal := range vals {
-		switch val := rawVal.(type) {
-		case Diagnostics:
-			diags = append(diags, val...)
-		case Diagnostic:
-			diags = append(diags, val)
-		case error:
-			// We'll generate a generic error diagnostic then, to more easily
-			// adapt from existing APIs that deal only in errors.
-			diags = append(diags, Diagnostic{
-				Severity: Error,
-				Summary:  "Error from provider",
-				Detail:   fmt.Sprintf("Provider error: %s", FormatError(val)),
-			})
-		default:
-			panic(fmt.Sprintf("Diagnostics.Append does not support %T", rawVal))
-		}
-	}
-	return diags
-}
-
-func (diags Diagnostics) HasErrors() bool {
-	for _, diag := range diags {
-		if diag.Severity == Error {
-			return true
-		}
-	}
-	return false
-}
-
-// UnderPath rewrites the Path fields of the receiving diagnostics to be
-// relative to the given path. This can be used to gradually build up
-// a full path while working backwards from leaf values, avoiding the
-// need to pass full paths throughout validation and other processing
-// walks.
-//
-// This function modifies the reciever in-place, but also returns the receiver
-// for convenient use in function return statements.
-func (diags Diagnostics) UnderPath(base cty.Path) Diagnostics {
-	for i, diag := range diags {
-		path := make(cty.Path, 0, len(base)+len(diag.Path))
-		path = append(path, base...)
-		path = append(path, diag.Path...)
-		diags[i].Path = path
-	}
-	return diags
-}
-
-type DiagSeverity int
+type DiagSeverity = sdkdiags.DiagSeverity
 
 const (
-	diagSeverityInvalid DiagSeverity = iota
-
 	// Error is a diagnostic severity used to indicate that an option could
 	// not be completed as requested.
-	Error
+	Error = sdkdiags.Error
 
 	// Warning is a diagnostic severity used to indicate a problem that
 	// did not block the competion of the requested operation but that the
 	// user should be aware of nonetheless.
-	Warning
+	Warning = sdkdiags.Warning
 )
 
 // FormatError returns a string representation of the given error. For most
 // error types this is equivalent to calling .Error, but will augment a
 // cty.PathError by adding the indicated attribute path as a prefix.
 func FormatError(err error) string {
-	switch tErr := err.(type) {
-	case cty.PathError:
-		if len(tErr.Path) == 0 {
-			// No prefix to render, then
-			return tErr.Error()
-		}
-
-		return fmt.Sprintf("%s: %s", FormatPath(tErr.Path), tErr.Error())
-	default:
-		return err.Error()
-	}
+	return sdkdiags.FormatError(err)
 }
 
 // FormatPath returns a string representation of the given path using a syntax
 // that resembles an expression in the Terraform language.
 func FormatPath(path cty.Path) string {
-	var buf strings.Builder
-	for _, rawStep := range path {
-		switch step := rawStep.(type) {
-		case cty.GetAttrStep:
-			fmt.Fprintf(&buf, ".%s", step.Name)
-		case cty.IndexStep:
-			switch step.Key.Type() {
-			case cty.String:
-				// fmt's %q isn't quite the same as Terraform quoted string syntax,
-				// but it's close enough for error reporting.
-				fmt.Fprintf(&buf, "[%q]", step.Key.AsString())
-			case cty.Number:
-				fmt.Fprintf(&buf, "[%s]", step.Key.AsBigFloat())
-			default:
-				// A path through a set can contain a key of any type in principle,
-				// but it will never be anything we can render compactly in a
-				// path expression string, so we'll just use a placeholder.
-				buf.WriteString("[...]")
-			}
-		default:
-			// Should never happen because there are no other step types
-			buf.WriteString(".(invalid path step)")
-		}
-	}
-	return buf.String()
+	return sdkdiags.FormatPath(path)
 }
 
 // ValidationError is a helper for constructing a Diagnostic to report an
@@ -156,17 +64,7 @@ func FormatPath(path cty.Path) string {
 // be the case automatically if the cty.Value passed to the ValidateFn is used
 // with functions from the cty "convert" and "gocty" packages.
 func ValidationError(err error) Diagnostic {
-	var path cty.Path
-	if pErr, ok := err.(cty.PathError); ok {
-		path = pErr.Path
-	}
-
-	return Diagnostic{
-		Severity: Error,
-		Summary:  "Unsuitable argument value",
-		Detail:   fmt.Sprintf("This value cannot be used: %s.", FormatError(err)),
-		Path:     path,
-	}
+	return sdkdiags.ValidationError(err)
 }
 
 // UpstreamAPIError is a helper for constructing a Diagnostic to report an
@@ -187,9 +85,5 @@ func ValidationError(err error) Diagnostic {
 // Do not use this function for errors returned from local computation functions,
 // such as parsers, serializers, private key generators, etc.
 func UpstreamAPIError(err error) Diagnostic {
-	return Diagnostic{
-		Severity: Error,
-		Summary:  "Remote operation failed",
-		Detail:   fmt.Sprintf("The remote API returned an error that the provider was unable to handle:\n\n%s", err),
-	}
+	return sdkdiags.UpstreamAPIError(err)
 }
