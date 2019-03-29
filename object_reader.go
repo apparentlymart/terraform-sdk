a9c4d408580cf176208f16d05d82798003fafea5
Martin Atkins <mart@degeneration.co.uk>
2019-03-29 08:42:41 -0700

    Implement ObjectReader in terms of the cty.Value API
    This API is already pretty similar to the ObjectReader API, so this just
    boils down to some additional safety checks to represent the attribute vs.
    block distinction that cty itself doesn't know about.

--- object_reader.go
+++ object_reader.go
@@ -1,6 +1,8 @@
 package tfsdk
 
 import (
+	"fmt"
+
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -20,6 +22,10 @@ type ObjectReader interface {
 	// in its schema.
 	Attr(name string) cty.Value
 
+	// BlockCount returns the number of blocks present of the given type, or
+	// panics if the given name isn't declared as a block type in the schema.
+	BlockCount(blockType string) int
+
 	// The "Block..." family of methods all interact with nested blocks.
 	//
 	// BlockSingle, BlockList, and BlockMap allow reading all of the blocks of
@@ -38,4 +44,145 @@ type ObjectReader interface {
 	BlockMap(blockType string) map[string]ObjectReader
 	BlockFromList(blockType string, idx int) ObjectReader
 	BlockFromMap(blockType string, key string) ObjectReader
+}
+
+// ObjectReader constructs a new ObjectReader for reading the given object
+// value, which must be a non-null, known value whose type conforms to the
+// implied type of the recieving schema, or the results are undefined.
+func (s *SchemaBlockType) ObjectReader(obj cty.Value) ObjectReader {
+	if obj.IsNull() || !obj.IsKnown() {
+		panic("ObjectReader called with object that isn't known and non-null")
+	}
+	if !obj.Type().IsObjectType() {
+		panic("ObjectReader called with non-object value")
+	}
+	return &objectReaderVal{
+		schema: s,
+		v:      obj,
+	}
+}
+
+type objectReaderVal struct {
+	schema *SchemaBlockType
+	v      cty.Value
+}
+
+var _ ObjectReader = (*objectReaderVal)(nil)
+
+func (r *objectReaderVal) Schema() *SchemaBlockType {
+	return r.schema
+}
+
+func (r *objectReaderVal) ObjectVal() cty.Value {
+	return r.v
+}
+
+func (r *objectReaderVal) Attr(name string) cty.Value {
+	_, exists := r.schema.Attributes[name]
+	if !exists {
+		panic(fmt.Sprintf("attempt to read non-attribute %q with Attr", name))
+	}
+	return r.v.GetAttr(name)
+}
+
+func (r *objectReaderVal) BlockCount(blockType string) int {
+	blockS, obj := r.blockVal(blockType)
+	switch blockS.Nesting {
+	case SchemaNestingSingle:
+		return 1
+	default:
+		if obj.IsNull() || !obj.IsKnown() {
+			// Should never happen when Terraform is behaving itself, but
+			// we'll be robust to avoid a crash here.
+			return 0
+		}
+		return obj.LengthInt()
+	}
+}
+
+func (r *objectReaderVal) BlockSingle(blockType string) ObjectReader {
+	blockS, obj := r.blockVal(blockType)
+	if blockS.Nesting != SchemaNestingSingle {
+		panic(fmt.Sprintf("attempt to read block type %q (%s) with BlockSingle method", blockType, blockS.Nesting))
+	}
+	return &objectReaderVal{
+		schema: &blockS.Content,
+		v:      obj,
+	}
+}
+
+func (r *objectReaderVal) BlockList(blockType string) []ObjectReader {
+	blockS, list := r.blockVal(blockType)
+	if blockS.Nesting != SchemaNestingList && blockS.Nesting != SchemaNestingSet {
+		panic(fmt.Sprintf("attempt to read block type %q (%s) with BlockList method", blockType, blockS.Nesting))
+	}
+	if list.IsNull() || !list.IsKnown() {
+		// Should never happen when Terraform is behaving itself, but
+		// we'll be robust to avoid a crash here.
+		return nil
+	}
+	l := list.LengthInt()
+	ret := make([]ObjectReader, 0, l)
+	for it := list.ElementIterator(); it.Next(); {
+		_, v := it.Element()
+		ret = append(ret, &objectReaderVal{
+			schema: &blockS.Content,
+			v:      v,
+		})
+	}
+	return ret
+}
+
+func (r *objectReaderVal) BlockMap(blockType string) map[string]ObjectReader {
+	blockS, m := r.blockVal(blockType)
+	if blockS.Nesting != SchemaNestingMap {
+		panic(fmt.Sprintf("attempt to read block type %q (%s) with BlockMap method", blockType, blockS.Nesting))
+	}
+	if m.IsNull() || !m.IsKnown() {
+		// Should never happen when Terraform is behaving itself, but
+		// we'll be robust to avoid a crash here.
+		return nil
+	}
+	l := m.LengthInt()
+	ret := make(map[string]ObjectReader, l)
+	for it := m.ElementIterator(); it.Next(); {
+		k, v := it.Element()
+		ret[k.AsString()] = &objectReaderVal{
+			schema: &blockS.Content,
+			v:      v,
+		}
+	}
+	return ret
+}
+
+func (r *objectReaderVal) BlockFromList(blockType string, idx int) ObjectReader {
+	blockS, list := r.blockVal(blockType)
+	if blockS.Nesting != SchemaNestingList {
+		panic(fmt.Sprintf("attempt to read block type %q (%s) with BlockFromList method", blockType, blockS.Nesting))
+	}
+	v := list.Index(cty.NumberIntVal(int64(idx)))
+	return &objectReaderVal{
+		schema: &blockS.Content,
+		v:      v,
+	}
+}
+
+func (r *objectReaderVal) BlockFromMap(blockType string, key string) ObjectReader {
+	blockS, list := r.blockVal(blockType)
+	if blockS.Nesting != SchemaNestingMap {
+		panic(fmt.Sprintf("attempt to read block type %q (%s) with BlockFromMap method", blockType, blockS.Nesting))
+	}
+	v := list.Index(cty.StringVal(key))
+	return &objectReaderVal{
+		schema: &blockS.Content,
+		v:      v,
+	}
+}
+
+func (r *objectReaderVal) blockVal(blockType string) (*SchemaNestedBlockType, cty.Value) {
+	blockS, exists := r.schema.NestedBlockTypes[blockType]
+	if !exists {
+		panic(fmt.Sprintf("attempt to read non-block-type %q with block method", blockType))
+	}
+	return blockS, r.v.GetAttr(blockType)
 }
