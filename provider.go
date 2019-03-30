b1e1ddcf9b42bb3340cae9cf64ac7bb85ad9fdb4
Martin Atkins <mart@degeneration.co.uk>
2019-03-30 14:13:38 -0700

    Unexport all of the Provider methods
    Initially I thought that these would need to be exported because I was
    going to put the wire protocol implementation in another package. However,
    it ended up feeling at home in the main package, and so these methods now
    have no reason to be called by anything except the wire protocol
    implementation and can therefore be unexported.
    
    Having these be private will, I expect, be important as we evolve the
    provider wire protocol since the Provider method signatures currently
    depend on some details about the protocol design. By having these
    unexported, we can lean entirely on the protocol buffers and go-plugin
    versioning mechanisms to manage changes over time, and freely change these
    internal method signatures as needed to support those changes.

--- provider.go
+++ provider.go
@@ -51,19 +51,19 @@ type DataResourceType interface {
 	read(ctx context.Context, client interface{}, config cty.Value) (cty.Value, Diagnostics)
 }
 
-// PrepareConfig accepts an object decoded from the user-provided configuration
+// prepareConfig accepts an object decoded from the user-provided configuration
 // (whose type must conform to the schema) and validates it, possibly also
 // altering some of the values within to produce a final configuration for
 // Terraform Core to use when interacting with this provider instance.
-func (p *Provider) PrepareConfig(proposedVal cty.Value) (cty.Value, Diagnostics) {
+func (p *Provider) prepareConfig(proposedVal cty.Value) (cty.Value, Diagnostics) {
 	diags := ValidateBlockObject(p.ConfigSchema, proposedVal)
 	return proposedVal, diags
 }
 
-// Configure recieves the finalized configuration for the provider and passes
+// configure recieves the finalized configuration for the provider and passes
 // it to the provider's configuration function to produce the client object
 // that will be recieved by the various resource operations.
-func (p *Provider) Configure(ctx context.Context, config cty.Value) Diagnostics {
+func (p *Provider) configure(ctx context.Context, config cty.Value) Diagnostics {
 	var diags Diagnostics
 	var client interface{}
 	fn, err := dynfunc.WrapFunctionWithReturnValue(p.ConfigureFn, &client, ctx, config)
@@ -84,26 +84,26 @@ func (p *Provider) Configure(ctx context.Context, config cty.Value) Diagnostics 
 	return diags
 }
 
-func (p *Provider) ManagedResourceType(typeName string) ManagedResourceType {
+func (p *Provider) managedResourceType(typeName string) ManagedResourceType {
 	return p.ManagedResourceTypes[typeName]
 }
 
-func (p *Provider) DataResourceType(typeName string) DataResourceType {
+func (p *Provider) dataResourceType(typeName string) DataResourceType {
 	return p.DataResourceTypes[typeName]
 }
 
-func (p *Provider) ReadResource(ctx context.Context, rt ManagedResourceType, currentVal cty.Value) (cty.Value, Diagnostics) {
+func (p *Provider) readResource(ctx context.Context, rt ManagedResourceType, currentVal cty.Value) (cty.Value, Diagnostics) {
 	return rt.refresh(ctx, p.client, currentVal)
 }
 
-func (p *Provider) ReadDataSource(ctx context.Context, rt DataResourceType, configVal cty.Value) (cty.Value, Diagnostics) {
+func (p *Provider) readDataSource(ctx context.Context, rt DataResourceType, configVal cty.Value) (cty.Value, Diagnostics) {
 	return rt.read(ctx, p.client, configVal)
 }
 
-func (p *Provider) PlanResourceChange(ctx context.Context, rt ManagedResourceType, priorVal, configVal, proposedVal cty.Value) (cty.Value, Diagnostics) {
+func (p *Provider) planResourceChange(ctx context.Context, rt ManagedResourceType, priorVal, configVal, proposedVal cty.Value) (cty.Value, Diagnostics) {
 	return rt.planChange(ctx, p.client, priorVal, configVal, proposedVal)
 }
 
-func (p *Provider) ApplyResourceChange(ctx context.Context, rt ManagedResourceType, priorVal, plannedVal cty.Value) (cty.Value, Diagnostics) {
+func (p *Provider) applyResourceChange(ctx context.Context, rt ManagedResourceType, priorVal, plannedVal cty.Value) (cty.Value, Diagnostics) {
 	return rt.applyChange(ctx, p.client, priorVal, plannedVal)
 }
