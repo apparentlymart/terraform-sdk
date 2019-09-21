ea6612ba3be6e3211bdf464e8bc51dbecee9a6ad
Martin Atkins <mart@degeneration.co.uk>
2019-09-21 10:43:08 -0700

    tftest: Remove SDK dependencies
    Having the tftest package depend on individual SDKs is problematic because
    it can cause more than one SDK to be loaded into a process at once, even
    though only one should be in use at a time.
    
    tftest was only importing the SDK to offer its helper for initializing a
    test, so we'll instead move that into the main SDK package and export the
    SDK-agnostic parts it was built from.
    
    This also involved removing the unfinished legacy SDK glue code from the
    main tfsdk package, because again otherwise we end up with two SDKs in
    the program at once and they both try to take control of the tfplugin5
    grpc service. Will need to find some other way to meld in legacy resource
    types into a modern provider.

--- terraform-provider-test/test/provider_test.go
+++ terraform-provider-test/test/provider_test.go
@@ -4,13 +4,14 @@ import (
 	"os"
 	"testing"
 
+	tfsdk "github.com/apparentlymart/terraform-sdk"
 	"github.com/apparentlymart/terraform-sdk/tftest"
 )
 
 var testHelper *tftest.Helper
 
 func TestMain(m *testing.M) {
-	testHelper = tftest.InitProvider("test", Provider())
+	testHelper = tfsdk.InitProviderTesting("test", Provider())
 	status := m.Run()
 	testHelper.Close()
 	os.Exit(status)
--- tftest/plugin.go
+++ tftest/plugin.go
@@ -4,10 +4,10 @@ import (
 	"os"
 )
 
-// runningAsPlugin returns true if it detects the usual Terraform plugin
+// RunningAsPlugin returns true if it detects the usual Terraform plugin
 // detection environment variables, suggesting that the current process is
 // being launched as a plugin server.
-func runningAsPlugin() bool {
+func RunningAsPlugin() bool {
 	const cookieVar = "TF_PLUGIN_MAGIC_COOKIE"
 	const cookieVal = "d602bf8f470bc67ca7faa0386276bbdd4330efaf76d1a219cb4d6991ca9872b2"
 
